d55d057a35e988ebd72f818212252074d52fce49
Beulla <beullarugero6@gmail.com>
2023-11-22 13:12:05 +0200

    hide the navbar on link click

--- src/components/Header/TheNavbar.js
+++ src/components/Header/TheNavbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavCartButton from "./NavCartButton";
 import { Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,7 +7,41 @@ import classes from "./TheNavbar.module.css";
 import Logo from "../../assets/Logo/logo-full.svg";
 
 const TheNavbar = (props) => {
-  //Layout and structure of the navbar to be passed to THENAV component
+  const [linksVisible, setLinksVisibility] = useState(true);
+
+  useEffect(() => {
+    const isMobileDevice = () => {
+      return window.innerWidth <= 800;
+    };
+    if (isMobileDevice()) {
+      setLinksVisibility(false);
+    }
+    const handleResize = () => {
+      if (isMobileDevice()) {
+        setLinksVisibility(false);
+      } else {
+        setLinksVisibility(true);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleLinkClick = () => {
+    if (window.innerWidth <= 800) {
+      setLinksVisibility(false);
+    }
+  };
+
+  const handleToggleClick = () => {
+    if (window.innerWidth <= 800) {
+      setLinksVisibility(true);
+    }
+  };
+
   return (
     <>
       <Navbar
@@ -25,80 +59,87 @@ const TheNavbar = (props) => {
         <Navbar.Toggle
           aria-controls="basic-navbar-nav"
           className={classes.toggle}
+          onClick={handleToggleClick}
         />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className={`${classes.nav__linkgroup} ms-auto`}>
-            <Nav.Link
-              className={`${classes.nav__link} ${classes.firstnav__link} me-4`}
-            >
-              <Link
-                activeClass={classes.active}
-                to="hero"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={500}
+          {linksVisible && (
+            <Nav className={`${classes.nav__linkgroup} ms-auto`}>
+              <Nav.Link
+                className={`${classes.nav__link} ${classes.firstnav__link} me-4`}
               >
-                Home
-              </Link>
-            </Nav.Link>
-            <Nav.Link className={`${classes.nav__link} me-4`}>
-              <Link
-                activeClass={classes.active}
-                to="why"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={500}
-              >
-                Why choose us
-              </Link>
-            </Nav.Link>
-            <Nav.Link className={`${classes.nav__link} me-4`}>
-              <Link
-                activeClass={classes.active}
-                to="dishes"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={500}
-              >
-                Our dishes
-              </Link>
-            </Nav.Link>
-            <Nav.Link className={`${classes.nav__link} me-4`}>
-              <Link
-                activeClass={classes.active}
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={500}
-              >
-                About us
-              </Link>
-            </Nav.Link>
-            <Nav.Link className={`${classes.nav__link} me-4`}>
-              <Link
-                activeClass={classes.active}
-                to="#"
-                spy={true}
-                smooth={true}
-                offset={-50}
-                duration={500}
-              >
-                Download
-              </Link>
-            </Nav.Link>
-            <Nav.Link href="/login" className={`${classes.nav__link}`}>
-              <NavCartButton onClick={props.onShowCart} />
-            </Nav.Link>
-          </Nav>
+                <Link
+                  activeClass={classes.active}
+                  to="hero"
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                  onClick={handleLinkClick}
+                >
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link className={`${classes.nav__link} me-4`}>
+                <Link
+                  activeClass={classes.active}
+                  to="why"
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                  onClick={handleLinkClick}
+                >
+                  Why choose us
+                </Link>
+              </Nav.Link>
+              <Nav.Link className={`${classes.nav__link} me-4`}>
+                <Link
+                  activeClass={classes.active}
+                  to="dishes"
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                  onClick={handleLinkClick}
+                >
+                  Our partners
+                </Link>
+              </Nav.Link>
+              <Nav.Link className={`${classes.nav__link} me-4`}>
+                <Link
+                  activeClass={classes.active}
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                  onClick={handleLinkClick}
+                >
+                  About us
+                </Link>
+              </Nav.Link>
+              <Nav.Link className={`${classes.nav__link} me-4`}>
+                <Link
+                  activeClass={classes.active}
+                  to="#"
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={500}
+                  onClick={handleLinkClick}
+                >
+                  Download
+                </Link>
+              </Nav.Link>
+              <Nav.Link href="/login" className={`${classes.nav__link}`}>
+                <NavCartButton onClick={props.onShowCart} />
+              </Nav.Link>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Navbar>
     </>
   );
-  //ENDS
 };
 
 export default TheNavbar;
